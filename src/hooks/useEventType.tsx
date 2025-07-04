d94e1ab4ff1c8c4ad1ba46633164e57c82248e67
Luiz Gustavo Ribeiro <lgustavor@hotmail.com>
2025-07-04 08:29:30 -0300

    Alteracao para 50 o pagesize do eventtype deploy

--- src/hooks/useEventType.tsx
+++ src/hooks/useEventType.tsx
@@ -10,7 +10,7 @@ export const useEventType = () => {
 
   const fetchEventType = async (
     page: number = 1,
-    pageSize: number = 10,
+    pageSize: number = 50,
     searchTerm: string = ''
   ) => {
     try {
